87a4abeea84977f1900bf811f819f08ad8621e62
13040 <1304041493@qq.com>
2025-12-14 14:11:04 +0800

    前端项目初始化

---
+++ src/views/LoginSignUp.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<div class="login">
+		<form action="">
+			<input type="text">
+			<input type="text">
+			<button>登录</button>
+		</form>
+	</div>
+</template>
+
+<style scoped>
+	.login{
+		display: flex;
+	}
+</style>
